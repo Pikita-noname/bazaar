33967093a4ce87986c7b1acf8354feefb21000d4
ndk <n.davydov@12.digital>
2024-10-15 23:26:22 +1000

    put port in .env

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,14 +1,17 @@
 import express, { Request, Response } from "express";
 import cors from "cors";
 import "reflect-metadata";
+import dotenv from "dotenv";
 
 import basketRouter from "./routes/basket";
 import orderRouter from "./routes/order";
 import userRouter from "./routes/user";
 import reviewRouter from "./routes/review";
 
+dotenv.config();
+
 const app = express();
-const port = 1234;
+const port = process.env.PORT;
 
 app.use(cors());
 app.use(express.json());
